7acf76c8c7876763768c98fe16d1cacb561d02f8
songjiyuan1994 <songjiyuan1994@icloud.com>
2018-03-08 14:22:54 +0800

    标题栏

--- js/screens/DetailScreen.js
+++ js/screens/DetailScreen.js
@@ -2,9 +2,12 @@ import React, {Component} from 'react';
 import {
     StyleSheet,
     View,
-    Text
+    Text,
+    Image,
+    TouchableOpacity,
 } from 'react-native';
 import NavigationBar from "../view/NavigationBar";
+
 export default class DetailScreen extends Component<Props> {
     constructor(props) {
         super(props)
@@ -14,7 +17,19 @@ export default class DetailScreen extends Component<Props> {
         return (
             <View style={{flex: 1}}>
                 <NavigationBar
-                    title={'boy'}
+                    title={'详情'}
+                    style={{
+                        backgroundColor: '#ee6363'
+                    }}
+                    leftButton={
+                        <TouchableOpacity
+                            onPress={() => {
+                                this.props.navigation.goBack(null);
+                            }}>
+                            <Image source={require('../../res/images/ic_arrow_back_white_36pt.png')}
+                            />
+                        </TouchableOpacity>
+                    }
                 />
                 <Text style={{fontSize: 20}}>detail</Text>
             </View>
--- js/view/NavigationBar.js
+++ js/view/NavigationBar.js
@@ -9,10 +9,14 @@ import {
 } from 'react-native';
 import PropTypes from 'prop-types';
 
-const NAV_BAR_HEIGHT_NADROID = 50;
+const NAV_BAR_HEIGHT_ANDROID = 50;
 const NAV_BAR_HEIGHT_IOS = 44;
 const STATUS_BAR_HEIGHT = 20;
-
+const StatusBarShape = {
+    backgroundColor: PropTypes.string,
+    barStyle: PropTypes.oneOf(['default', 'light-content', 'dark-content']),
+    hidden: PropTypes.bool
+};
 export default class NavigationBar extends Component<Props> {
     static propTypes = {
         style: View.propTypes.style,
@@ -21,34 +25,45 @@ export default class NavigationBar extends Component<Props> {
         hide: PropTypes.bool,
         leftButton: PropTypes.element,
         rightButton: PropTypes.element,
+        statusBar: PropTypes.shape(StatusBarShape),
+    };
+    static defaultProps = {
+        statusBar: {
+            barStyle: 'light-content',
+            hidden: false
+        }
     };
 
     constructor(props) {
         super(props);
         this.state = {
-            title: '',
+            title: '标题',
             hide: false
         };
     }
 
     render() {
+        let status = <View style={[navStyle.statusBar, this.props.statusBar]}>
+            <StatusBar {...this.props.statusBar}/>
+        </View>;
         let titleView = this.props.titleView ? this.props.titleView :
-            <Text style={navBar.title}>{this.props.title}</Text>;
-        let content = <View style={navBar.navBar}>
+            <Text style={navStyle.title}>{this.props.title}</Text>;
+        let content = <View style={navStyle.navBar}>
             {this.props.leftButton}
-            <View style={navBar.titleViewContainer}>
+            <View style={navStyle.titleViewContainer}>
                 {titleView}
             </View>
             {this.props.rightButton}
         </View>;
         return (
-            <View style={navBar.container}>
+            <View style={[navStyle.container, this.props.style]}>
+                {status}
                 {content}
             </View>
         );
     }
 }
-const navBar = StyleSheet.create({
+const navStyle = StyleSheet.create({
     container: {
         backgroundColor: 'gray'
     },
@@ -56,11 +71,12 @@ const navBar = StyleSheet.create({
         justifyContent: 'space-between',
         alignItems: 'center',
         height: Platform.OS === 'ios' ?
-            NAV_BAR_HEIGHT_IOS : NAV_BAR_HEIGHT_NADROID,
+            NAV_BAR_HEIGHT_IOS : NAV_BAR_HEIGHT_ANDROID,
         flexDirection: 'row'
     },
     title: {
-        fontSize: 20
+        fontSize: 20,
+        color: '#fff'
     },
     titleViewContainer: {
         justifyContent: 'center',
@@ -70,5 +86,8 @@ const navBar = StyleSheet.create({
         right: 40,
         top: 0,
         bottom: 0
+    },
+    statusBar: {
+        height: Platform.OS === 'ios' ? STATUS_BAR_HEIGHT : 0
     }
 });
